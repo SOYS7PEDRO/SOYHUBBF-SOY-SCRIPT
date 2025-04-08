a0cc24e9a9df838cc1d65792902f3ddf8ff2d9d2
SOYS7PEDRO <ribeirotorres13@gmail.com>
2025-04-08 20:59:46 -0300

    Create tabs.lua

---
+++ core/tabs.lua
@@ -0,0 +1,17 @@
+-- Arquivo core/tabs.lua
+-- Aqui futuramente vamos colocar as abas do menu principal (Farm, Boss, Eventos etc)
+
+local Tabs = {}
+
+-- Exemplo de função de aba
+function Tabs.LoadFarmTab()
+    print("Aba de Farm carregada.")
+end
+
+function Tabs.LoadBossTab()
+    print("Aba de Boss carregada.")
+end
+
+-- Você pode adicionar mais funções para outras abas aqui...
+
+return Tabs
